17568b0d61c3a4369d1c4c268a8a7f638861a62f
Shahria Jaman Khan <41187226+shahria7k@users.noreply.github.com>
2021-02-09 14:55:33 -0800

    home changed

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class MainWindow(QMainWindow):
         self.browser.urlChanged.connect(self.update_url)
 
     def navigate_home(self):
-        self.browser.setUrl(QUrl('http://programming-hero.com'))
+        self.browser.setUrl(QUrl('http://digitalgarage.space'))
 
     def navigate_to_url(self):
         url = self.url_bar.text()
